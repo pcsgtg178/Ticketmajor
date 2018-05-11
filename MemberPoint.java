6f6c92de6833de4c95df3f92fa12bdfe9050fd63
pcsgtg178 <34190810+pcsgtg178@users.noreply.github.com>
2018-05-11 12:57:13 +0700

    Update MemberPoint.java

--- MemberPoint.java
+++ MemberPoint.java
@@ -2,13 +2,11 @@ import javax.swing.*;
 public class MemberPoint {
 
 	public static void main(String[] args) {
-		float point = 0,money = 0;
+		double point = 0,money = 0;
 		String input;
 		
-		Calpoint calpoint = new Calpoint();
-		
 		input = JOptionPane.showInputDialog(null,"PLease enter Money :");
-		money = Float.parseFloat(input);
+		money = Double.parseDouble(input);
 		
 		point = calpoint.calpoint( money);
 		
